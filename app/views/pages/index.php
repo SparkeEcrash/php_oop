11c5a40c2c713eaf7640d140e101ebe8265b3bee
SparkeEcrash <jpark121989@gmail.com>
2019-03-09 17:33:40 -0800

    add MVC framework

---
+++ app/views/pages/index.php
@@ -0,0 +1,7 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+
+	<h1><?php echo $data['title']?></h1>
+	<p>This is the PHP_OOP MVC Framework</p>
+
+<?php require APPROOT . '/views/inc/footer.php'; ?>
+
